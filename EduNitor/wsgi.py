84af2cf1759001c34b793e5ad241031d921dcd85
Pramod Topannavar <pramodtopannavar33@gmail.com>
2025-04-16 23:07:19 +0530

    First commit on project

--- EduNitor/wsgi.py
+++ EduNitor/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyRemoteDesk.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EduNitor.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyRemoteDesk.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EduNitor.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
